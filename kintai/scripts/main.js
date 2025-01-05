d24dccbed4e2051ecf63a1d83cb25c3ed0b01fae
Yuya, Matsumoto <yuya2005m@gmail.com>
2025-01-05 09:13:06 +0900

    fix and add kintai page

---
+++ kintai/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.interview__slider').slick({
+        infinite: false,
+        variableWidth: true,
+        infinite: true,
+        centerMode: true,
+        dots: true,
+    });
+});
